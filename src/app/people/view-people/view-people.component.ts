c93599a1395b31036613f07f00eb57b617a82c82
Chandrayog Yadav <chandrayog.2@gmail.com>
2021-06-09 11:39:42 -0300

    Commenting

--- src/app/people/view-people/view-people.component.ts
+++ src/app/people/view-people/view-people.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild, OnInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 import { PeopleService } from 'src/app/services/people.service';
 
 @Component({
@@ -10,27 +12,41 @@ export class ViewPeopleComponent implements OnInit {
   submitted = false;
   loading = false;
   people = [];
+  dataSource: any;
   constructor(private peopleService: PeopleService) {}
 
   ngOnInit(): void {
     // this.peopleService.GetRequest().subscribe((response: any) => {
     //   this.people = response;
     // });
-    this.peopleService.GetRequest().subscribe(
-      (response) => {
-        this.people = response.results;
-        console.log(response);
-      },
-      (error) => {
-        console.log('error: ');
-        console.log(error);
-        this.loading = false;
-      },
-      () => {
-        this.loading = false;
-        console.log('completed');
-        //this.router.navigateByUrl('/people/view');
-      }
-    );
-  }
+
+
+//   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+//   ngAfterViewInit() {
+//     this.dataSource.paginator = this.paginator;
+//   }
+//     this.peopleService.GetRequest().subscribe(
+//       (response) => {
+//        // this.people = response.results;
+//         this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+//         console.log(response);
+//       },
+//       (error) => {
+//         console.log('error: ');
+//         console.log(error);
+//         this.loading = false;
+//       },
+//       () => {
+//         this.loading = false;
+//         console.log('completed');
+//         //this.router.navigateByUrl('/people/view');
+//       }
+//     );
+//   }
+//   GetDetails = function (index) {
+//     var name = $scope.Customers[index].Name;
+//     var country = $scope.Customers[index].Country;
+//     $window.alert("Name: " + name + "\nCountry: " + country);
+// };
 }
